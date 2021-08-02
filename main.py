e3e038d0204bd15731fc52af76bf341399c38354
psick22 <kgirok@gmail.com>
2021-08-03 01:12:59 +0900

    first binance client

---
+++ main.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+import logging
+
+from binanace_future import BinanceFuturesClient
+
+logger = logging.getLogger()
+logger.debug("debug mode")
+logger.info("basic information")
+logger.warning("warning")
+logger.error("error")
+
+logger.setLevel(logging.INFO)
+
+stream_handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s [%(levelname)s] | %(message)s')
+stream_handler.setFormatter(formatter)
+stream_handler.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler("info.log")
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
+
+if __name__ == '__main__':
+    binance = BinanceFuturesClient(True)
+    root = tk.Tk()
+    root.mainloop()
